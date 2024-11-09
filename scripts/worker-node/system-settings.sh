16f5efeaa57ef9daceb0c3bd1bc27a25e2eb8d79
kukv <rio.by.kukv@gmail.com>
2024-11-10 02:51:13 +0900

    wip

--- scripts/worker-node/system-settings.sh
+++ scripts/worker-node/system-settings.sh
@@ -1,18 +1,3 @@
-# k3sからiptablesでブリッジのトラフィックを制御できるよう設定
-cat <<EOF >/etc/modules-load.d/k8s.conf
-br_netfilter
-EOF
-
-cat <<EOF >/etc/sysctl.d/k8s.conf
-net.bridge.bridge-nf-call-ip6tables = 1
-net.bridge.bridge-nf-call-iptables = 1
-EOF
-
-sysctl --system
-
-# パッケージの最新化
-apt update && apt -y upgrade
-
 apt install -y \
   open-iscsi \
   nfs-common \
