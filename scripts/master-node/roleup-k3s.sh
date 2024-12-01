0231fafc4039be7c21c740be8dcad4c73b3c209d
kukv <rio.by.kukv@gmail.com>
2024-12-01 21:39:33 +0900

    clean

--- scripts/master-node/roleup-k3s.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -euo pipefail
-cd "$(dirname "${0}")"
-
-kubectl label nodes br-node4 kubernetes.io/role=worker
-kubectl label nodes br-node5 kubernetes.io/role=worker
-kubectl label nodes br-node6 kubernetes.io/role=worker
